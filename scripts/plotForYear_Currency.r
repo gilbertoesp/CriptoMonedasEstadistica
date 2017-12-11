a8d8fce441a79bb00c3ae2f38e7119fbfcf58153
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2017-12-11 16:26:23 -0700

    Ploteando Open/Close High/Low

--- scripts/plotForYear_Currency.r
+++ scripts/plotForYear_Currency.r
@@ -10,6 +10,18 @@ BTC <- read.csv("csv/btc/bitcoin_price.csv")
 # Modificando las fechas para que estas sean comparables
 BTC[["Date"]] <- as.Date(BTC[["Date"]], format="%b %d,%Y")
 #-----------------------------------------------------
+# Reading files for the data frames
+ETH <- read.csv("csv/eth/ethereum_price.csv")
+# Modificando las fechas para que estas sean comparables
+ETH[["Date"]] <- as.Date(ETH[["Date"]], format="%b %d,%Y")
+#-----------------------------------------------------
+# Reading files for the data frames
+CASH <- read.csv("csv/other/bitcoin_cash_price.csv")
+# Modificando las fechas para que estas sean comparables
+CASH[["Date"]] <- as.Date(CASH[["Date"]], format="%b %d,%Y")
+#-----------------------------------------------------
+
+
 #Vamos a plotear con ggplot2
 require(ggplot2)
 
@@ -18,7 +30,7 @@ subset_date <- function(data,date_1, date_2){
 	# 	date_1 se incluye en el set, date_2 no
 	set <- subset(data, subset = ( date_1 <= data$Date & date_2 >data$Date) )
 	return(set)	
-}
+}	 
 
 subset_month_mean <- function(data,date_1, date_2){
 	months <- c("Jan", "Feb", "Mar", "Apr", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" )
@@ -26,6 +38,13 @@ subset_month_mean <- function(data,date_1, date_2){
 }
 
 set_2015 <- subset_date(BTC, "2015-01-01", "2016-01-01")
+
+fecha_minima_eth <- min(ETH$Date)
+fecha_minima_eth
+fecha_minima_bth <- min(CASH$Date)
+fecha_minima_bth
+
+
 # Plotting el subset
 # Este proceso no puede ser impletando en una funcion dado que no existe Low high, definidos
 # fuera del contexto que estas manipulando los frames
@@ -34,4 +53,39 @@ set_2015 <- subset_date(BTC, "2015-01-01", "2016-01-01")
 #plot_date(BTC, High, "2015-01-01","2016-01-01")
 #plot_date(BTC, Open, "2015-01-01","2016-01-01")
 #plot_date(BTC, Close, "2015-01-01","2016-01-01")
-ggplot(set_2015, aes(Date)) + geom_line(aes(y = High, color="GREEN")) + geom_line(aes(y = Low, color="RED")) + scale_color_manual(labels=c("High", "Low"),values=c("RED","BLUE"))
+ggplot(BTC, aes(Date)) + 
+	geom_line(aes(y = Open, color="GREEN")) + 
+	geom_line(aes(y = Close, color="RED")) + 
+	scale_color_manual(labels=c("Open", "Close"),values=c("GREEN","RED")) + 
+	labs(y="Precio USD", title="Bitcoin Open-Close")
+
+ggplot(ETH, aes(Date)) + 
+	geom_line(aes(y = Open, color="GREEN")) + 
+	geom_line(aes(y = Close, color="RED")) + 
+	scale_color_manual(labels=c("Open", "Close"),values=c("GREEN","RED")) + 
+	labs(y="Precio USD", title="Ethereum Open-Close")
+
+ggplot(CASH, aes(Date)) + 
+	geom_line(aes(y = Open, color="GREEN")) + 
+	geom_line(aes(y = Close, color="RED")) + 
+	scale_color_manual(labels=c("Open", "Close"),values=c("GREEN","RED")) + 
+	labs(y="Precio USD", title="Bitcoin Cash Open-Close")
+#---------------------------------------------------------------
+ggplot(BTC, aes(Date)) + 
+	geom_line(aes(y = Low, color="BLUE")) + 
+	geom_line(aes(y = High, color="YELLOW")) + 
+	scale_color_manual(labels=c("Low", "High"),values=c("BLUE","YELLOW")) + 
+	labs(y="Precio USD", title="Bitcoin Low-High")
+
+ggplot(ETH, aes(Date)) + 
+	geom_line(aes(y = Low, color="BLUE")) + 
+	geom_line(aes(y = High, color="YELLOW")) + 
+	scale_color_manual(labels=c("Low", "High"),values=c("BLUE","YELLOW")) + 
+	labs(y="Precio USD", title="Ethereum Low-High")
+
+ggplot(CASH, aes(Date)) + 
+	geom_line(aes(y = Low, color="BLUE")) + 
+	geom_line(aes(y = High, color="YELLOW")) + 
+	scale_color_manual(labels=c("Low", "High"),values=c("BLUE","YELLOW")) + 
+	labs(y="Precio USD", title="Bitcoin Cash Low-High")
+#---------------------------------------------------------------
